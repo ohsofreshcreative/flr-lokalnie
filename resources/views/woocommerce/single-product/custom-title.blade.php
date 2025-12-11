ff273c230fcc1a5f414c2bddf8c1e7e9ffe50df4
Arek <kontakt@ohsofresh.pl>
2025-12-11 11:50:12 +0100

    Automatyczny deploy

--- resources/views/woocommerce/single-product/custom-title.blade.php
+++ resources/views/woocommerce/single-product/custom-title.blade.php
@@ -1,20 +1,22 @@
-
 <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 mb-4">
-  <div>
-    <h2 class="product_title entry-title text-white">
-      {{ get_the_title() }}
+	<div>
+		<h2 class="product_title entry-title text-white">
+			{{ get_the_title() }}
 
-      @if ($custom_text)
-        <h3 class="primary">{{ esc_html($custom_text) }}</h3>
-      @endif
-    </h2>
-  </div>
+			@if ($custom_text)
+			@php
+			$event_date = date_i18n('j F Y', strtotime($custom_text));
+			@endphp
+			<h3 class="primary">{{ $event_date }}</h3>
+			@endif
+		</h2>
+	</div>
 
-  @if ($certificate_link && $certificate_img)
-    <div class="justify-self-start md:justify-self-end">
-      <a href="{{ esc_url($certificate_link) }}" target="_blank" rel="noopener">
-        <img class="" src="{{ esc_url($certificate_img) }}" alt="Certyfikat"/>
-      </a>
-    </div>
-  @endif
+	@if ($certificate_link && $certificate_img)
+	<div class="justify-self-start md:justify-self-end">
+		<a href="{{ esc_url($certificate_link) }}" target="_blank" rel="noopener">
+			<img class="" src="{{ esc_url($certificate_img) }}" alt="Certyfikat" />
+		</a>
+	</div>
+	@endif
 </div>
