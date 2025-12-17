4120458422a9dcbe0c85e37f45508953196c9768
Arek <kontakt@ohsofresh.pl>
2025-12-17 10:57:09 +0100

    Automatyczny deploy

--- patterns/woo-coming-soon.php
+++ patterns/woo-coming-soon.php
@@ -12,7 +12,7 @@
   <!-- wp:group {"layout":{"type":"constrained"}} -->
   <div class="wp-block-group" style="text-align:center;max-width:800px">
     <!-- wp:site-logo {"width":120} /-->
-	<img src="/wp-content/uploads/2025/11/logo.svg" />
+	<img src="{{ $logo['url'] }}" />
     <!-- wp:heading {"level":1, "style":{"color":{"text":"#ffffff"}}} -->
     <h1 class="wp-block-heading" style="color:#FFF">Wracamy wkrótce!</h1>
     <!-- /wp:heading -->
