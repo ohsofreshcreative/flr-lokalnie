93297adfaccb305440b89b4ce7a58570efba445a
Arek <kontakt@ohsofresh.pl>
2025-12-17 11:00:57 +0100

    Automatyczny deploy

--- patterns/woo-coming-soon.php
+++ patterns/woo-coming-soon.php
@@ -8,11 +8,18 @@
 ?>
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="min-height:100vh;display:grid;place-items:center;padding:48px;background-color:var(--bg);">
+<div class="wp-block-group bg-background" style="min-height:100vh;display:grid;place-items:center;padding:48px;">
   <!-- wp:group {"layout":{"type":"constrained"}} -->
   <div class="wp-block-group" style="text-align:center;max-width:800px">
     <!-- wp:site-logo {"width":120} /-->
-<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] ?? 'Logo' }}" class="w-auto h-12">
+    <?php
+    $logo = get_field('logo_white', 'option');
+    if ($logo) :
+        $url = $logo['url'];
+        $alt = $logo['alt'] ?: 'Logo';
+    ?>
+        <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($alt); ?>" class="w-auto h-12">
+    <?php endif; ?>
     <!-- wp:heading {"level":1, "style":{"color":{"text":"#ffffff"}}} -->
     <h1 class="wp-block-heading" style="color:#FFF">Wracamy wkrótce!</h1>
     <!-- /wp:heading -->
